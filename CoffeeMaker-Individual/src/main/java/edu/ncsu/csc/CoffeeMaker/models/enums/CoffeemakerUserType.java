1dad4782753510f470ed0c5f9bfd8a94d354faa1
gtbachel <gtbachel@ncsu.edu>
2023-04-04 19:15:01 -0400

    javadocs in enums

--- CoffeeMaker-Individual/src/main/java/edu/ncsu/csc/CoffeeMaker/models/enums/CoffeemakerUserType.java
+++ CoffeeMaker-Individual/src/main/java/edu/ncsu/csc/CoffeeMaker/models/enums/CoffeemakerUserType.java
@@ -1,5 +1,14 @@
 package edu.ncsu.csc.CoffeeMaker.models.enums;
 
+/**
+ * Enum for user type.
+ *
+ * @author finnt
+ *
+ */
 public enum CoffeemakerUserType {
-    Staff, Customer
+    /** Staff user type. **/
+    Staff,
+    /** Customer user type. **/
+    Customer
 }
--- CoffeeMaker-Individual/src/main/java/edu/ncsu/csc/CoffeeMaker/models/enums/Permissions.java
+++ CoffeeMaker-Individual/src/main/java/edu/ncsu/csc/CoffeeMaker/models/enums/Permissions.java
@@ -1,5 +1,24 @@
 package edu.ncsu.csc.CoffeeMaker.models.enums;
 
+/**
+ * Permission Enum for users. Add more to add more permissions.
+ *
+ * @author finnt
+ *
+ */
 public enum Permissions {
-    CanModifyInventory, CanEditRecipe, CanCreateRecipe, CanOrderRecipe, CanMakeRecipe, CanFulfillOrder, CanPickupOrder
+    /** Can make changes to the inventory. */
+    CanModifyInventory,
+    /** Can make changes to recipes. */
+    CanEditRecipe,
+    /** Can use a recipe to make an order */
+    CanCreateRecipe,
+    /** Can place an order */
+    CanOrderRecipe,
+    /** Can create a new recipe */
+    CanMakeRecipe,
+    /** Can fulfill an order */
+    CanFulfillOrder,
+    /** Can pickup an order that is made */
+    CanPickupOrder
 }
